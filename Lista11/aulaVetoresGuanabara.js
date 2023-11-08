c216d5c0170481dd4662242b96bc864b6e94b547
amandageovanna <amanda.sousa@sptech.school>
2023-11-07 21:33:16 -0300

    anotações da aula de vetores do Gustavo Guanabara

--- Lista11/aulaVetoresGuanabara.js
+++ Lista11/aulaVetoresGuanabara.js
@@ -31,7 +31,8 @@ var valores = [8, 1, 7, 4, 2, 9]
 for(var pos in valores) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]} `)
 }
-fazer busca dentro de um vetor: 
+
+// fazer busca dentro de um vetor: 
 
 var num = [5,8,2,9,3]
 var pos = num.indexOf(3) // em que posição o número 8 está?
